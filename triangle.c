cdb92765333d1ea654be206e744cc837b53783ee
fastestnoob <30358781+fastestnoob@users.noreply.github.com>
2017-07-30 01:27:45 +0300

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -11,12 +11,12 @@ int main(void)
     float c = get_float();
     bool i = valid_triangle(a, b, c);
     
-    if (valid_triangle == true)
+    if (valid_triangle(a,b,c) == true)   //     <-- added & removed -->: if (valid_triangle == true)
         {
             printf("triangle side lengths are %i", valid_triangle);
         }
 
-    if (valid_triangle == false)
+    if (valid_triangle(a,b,c) == false)  //     <-- added & removed -->: if (valid_triangle == false)
         {
             printf("triangle side lengths are %i", valid_triangle);
         }
@@ -39,8 +39,41 @@ if (a + b <= c) || (a + c <= b) || (c + b <= a)  // check that sum of any two si
     return true;         // if we passed both tests, result is true.
 }
     
+*/
+#######################################################################################
+Error message:
+########################################################################################
+triangle.c:14:24: error: comparison between pointer and integer ('bool
+      (*)(float, float, float)' and 'int') [-Werror]
+    if (valid_triangle == true)
+        ~~~~~~~~~~~~~~ ^  ~~~~
+triangle.c:16:52: error: format specifies type 'int' but the argument has
+      type 'bool (*)(float, float, float)' [-Werror,-Wformat]
+            printf("triangle side lengths are %i", valid_triangle);
+                                              ~~   ^~~~~~~~~~~~~~
+triangle.c:19:9: error: comparison of function 'valid_triangle' equal to a
+      null pointer is always false [-Werror,-Wtautological-pointer-compare]
+    if (valid_triangle == false)
+        ^~~~~~~~~~~~~~    ~~~~~
+triangle.c:19:9: note: prefix with the address-of operator to silence this
+      warning
+    if (valid_triangle == false)
+        ^
+        &
+triangle.c:21:52: error: format specifies type 'int' but the argument has
+      type 'bool (*)(float, float, float)' [-Werror,-Wformat]
+            printf("triangle side lengths are %i", valid_triangle);
+                                              ~~   ^~~~~~~~~~~~~~
+triangle.c:12:10: error: unused variable 'i' [-Werror,-Wunused-variable]
+    bool i = valid_triangle(a, b, c);
+         ^
+triangle.c:34:17: error: expected expression
+if (a + b <= c) || (a + c <= b) || (c + b <= a)  // check that sum o...
+                ^
+6 errors generated.
+make: *** [triangle] Error 1
+~/workspace/ $ 
 
-
-
+*/
 
 
